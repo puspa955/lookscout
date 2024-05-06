bf041dbdee176e07b8732a657a8e4527a28e5d5e
puspa <limbup503@gmail.com>
2024-05-06 14:03:29 +0545

    backend

--- backend/app.js
+++ backend/app.js
@@ -6,22 +6,14 @@ const contact = require("./routes/contact");
 require("./conn/conn");
 app.use(express.json());
 app.use(cors({
-    origin:["https://lookscout-9krn.vercel.app/"],
+    origin:["https://lookscout-9krn.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
 
-  app.use("/api/v1", (req, res, next) => {
-    const allowedOrigins = ['https://lookscout-9krn.vercel.app/'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-app.listen("1000", () => {
-    console.log("Server started at port 1000");
+app.use("/api/v1", contact);
+const PORT = process.env.PORT || 1000; // Use the environment variable for the port or fallback to 1000
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
 });
